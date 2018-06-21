0032bd6f952f5fbb56175e345eade940e9cf323e
Nelson Campos <ncamposfh@gmail.com>
2018-06-21 12:19:25 -0300

    metodo put em users

--- users/users.router.ts
+++ users/users.router.ts
@@ -32,6 +32,22 @@ class UsersRouter extends Router {
       })
     })
 
+    application.put('/users/:id', (req, resp, next)=>{
+      const options  = {overwrite: true}
+      User.update({_id:req.params.id}, req.body, options)
+          .exec().then(result=>{
+            if(result.n){
+              return User.findById(req.params.id)
+            }else{
+              resp.send(404)
+            }
+      }).then(user=>{
+        resp.json(user)
+        return next()
+      })
+    })
+
+    
   }
 }
 
