0ffb7c149e61fa874e1ec0cefdf24f87b6e19931
Nelson Campos <ncamposfh@gmail.com>
2018-06-21 18:41:27 -0300

    api respondendo

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
+import * as corsMiddleware from 'restify-cors-middleware'
+
 import {environment} from '../common/environment'
 import {Router} from '../common/router'
-
-import * as fs from 'fs'
-import * as https from 'https'
+import {mergePatchBodyParser} from './merge-patch.parser'
+import {handleError} from './error.handler'
 
 export class Server {
 
@@ -17,29 +18,42 @@ export class Server {
     })
   }
 
-
   initRoutes(routers: Router[]): Promise<any>{
     return new Promise((resolve, reject)=>{
       try{
 
         this.application = restify.createServer({
           name: 'meat-api',
-          version: '1.0.0',
-          certificate: fs.readFileSync('./keys/cert.pem'),
-          key: fs.readFileSync('./keys/key.pem'),
+          version: '1.0.0'
         })
 
+        const corsOptions: corsMiddleware.Options = {
+          preflightMaxAge: 10,
+          origins: ['*'],
+          allowHeaders: ['*'],
+          exposeHeaders: ['x-custom-header']
+
+        }
+
+        const cors: corsMiddleware.CorsMiddleware = corsMiddleware(corsOptions)
+
+        this.application.pre(cors.preflight)
+
+        this.application.use(cors.actual)
         this.application.use(restify.plugins.queryParser())
         this.application.use(restify.plugins.bodyParser())
+        this.application.use(mergePatchBodyParser)
 
         //routes
         for (let router of routers) {
-          router.applyRoutes(this.application)  
+          router.applyRoutes(this.application)
         }
 
         this.application.listen(environment.server.port, ()=>{
            resolve(this.application)
         })
+
+        this.application.on('restifyError', handleError)
 
       }catch(error){
         reject(error)
@@ -48,8 +62,8 @@ export class Server {
   }
 
   bootstrap(routers: Router[] = []): Promise<Server>{
-      return this.initializeDb().then(() => 
-              this.initRoutes(routers).then(()=> this))  
+      return this.initializeDb().then(()=>
+             this.initRoutes(routers).then(()=> this))
   }
 
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,53 +1,46 @@
-import {Router} from '../common/router'
+import {ModelRouter} from '../common/model-router'
 import * as restify from 'restify'
+import {NotFoundError} from 'restify-errors'
 import {User} from './users.model'
 
-class UsersRouter extends Router {
+
+class UsersRouter extends ModelRouter<User> {
+
+  constructor(){
+    super(User)
+    this.on('beforeRender', document=>{
+      document.password = undefined
+      //delete document.password
+    })
+  }
+
+  findByEmail = (req, resp, next)=>{
+    if(req.query.email){
+      User.findByEmail(req.query.email)
+          .then(user => {
+            if(user){
+              return [user]
+            }else{
+              return []
+            }
+          })
+          .then(this.renderAll(resp, next))
+          .catch(next)
+    }else{
+      next()
+    }
+  }
+
   applyRoutes(application: restify.Server){
 
-    application.get('/users', (req, resp, next)=>{
-      User.find().then(users=>{
-        resp.json(users)
-        return next()
-      })
-    })
+    application.get({path:'/users', version: '2.0.0'}, [this.findByEmail, this.findAll])
+    application.get({path:'/users', version: '1.0.0'}, this.findAll)
+    application.get('/users/:id', [this.validateId, this.findById])
+    application.post('/users', this.save)
+    application.put('/users/:id', [this.validateId,this.replace])
+    application.patch('/users/:id', [this.validateId,this.update])
+    application.del('/users/:id', [this.validateId,this.delete])
 
-    application.get('/users/:id', (req, resp, next)=>{
-      User.findById(req.params.id).then(user=>{
-        if(user){
-          resp.json(user)
-          return next()
-        }
-        resp.send(404)
-        return next()
-      })
-    })
-
-    application.post('/users', (req, resp, next) => {
-      let user = new User(req.body)
-      user.save().then(user => {
-        user.password = undefined
-        resp.json(user)
-        return next()
-      })
-    })
-
-    application.put('/users/:id', (req, resp, next)=>{
-      const options  = {overwrite: true}
-      User.update({_id:req.params.id}, req.body, options)
-          .exec().then(result=>{
-            if(result.n){
-              return User.findById(req.params.id)
-            }else{
-              resp.send(404)
-            }
-      }).then(user=>{
-        resp.json(user)
-        return next()
-      })
-    })
-
-    
   }
 }
 
